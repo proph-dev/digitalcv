748fbb1b3a6fb25fd884b7f40d1b7738fb2901b3
theo-lemoine-oc <propjhh>
2023-04-12 23:14:20 +0200

    edit navbar

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,16 +1,19 @@
 import { Tab } from './Tab';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
 import navbar from './navbar.module.scss';
 
 export const Navbar = () => {
     return (
         <header className={ navbar.mainNavigation }>
             <nav>
+                <span>DigitalCV</span>
                 <ul>
                     <Tab url="/">Accueil</Tab>
                     <Tab url="/">Les CV digitaux</Tab>
                     <Tab url="/">Comment ça marche ?</Tab>
                     <li>
-                        Notifs
+                        <FontAwesomeIcon icon={ faBell } />
                     </li>
                     <Tab url="/">Se connecter</Tab>
                 </ul>
